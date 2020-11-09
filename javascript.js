d1b4e2e30165354f00f6951af391d1acb2e7e8a8
Forth <33458877+Forthtilliath@users.noreply.github.com>
2020-11-09 04:23:18 +0100

    fixe qqes erreurs js et border bot menu

--- javascript.js
+++ javascript.js
@@ -1,17 +1,16 @@
+var ele_menu = [
+	"accommodations",
+	"activities" ];
+	
 /****************************************************
  * Redirige la page vers une autre page ou vers une	*
  * 	ancre (dans ce cas, on active l'élement du menu	*
  ****************************************************/
 function goto(link) {
 	document.location.href = link;
-	// Si le lien correspond à un element du menu
-	switch(link) {
-		case "#accommodations" : 
-		case "#activities" :
-			// On lui ajoute la classe active
-			addActive(link.substring(1));
-			break;
-	}
+	
+	// Si le lien correspond à un element du menu, on l'active
+	addActive(link.substring(1));
 }
 
 /****************************************************
@@ -40,15 +39,18 @@ function getAnchor() {
  * Ajoute la classe active à un element du menu		*
  ****************************************************/
 function addActive(link) {
-	// On sélectionne tous les éléments du menu nav
-	var list_li = document.querySelectorAll("header > div > nav > ul > li");
-	// Pour chaque élément du menu
-	for (var i = 0; i < list_li.length; i++) {
-		// On retire la classe active
-		list_li[i].classList.remove("active");
+	// Si le lien fait parti du menu
+	if( ele_menu.includes(link) ) {
+		// On sélectionne tous les éléments du menu nav
+		var list_li = document.querySelectorAll("header > div > nav > ul > li");
+		// Pour chaque élément du menu
+		for (var i = 0; i < list_li.length; i++) {
+			// On retire la classe active
+			list_li[i].classList.remove("active");
+		}
+		// On ajoute la classe active correspondant au lien
+		document.getElementById("to_"+link).classList.add("active");
 	}
-	// On ajoute la classe active correspondant au lien
-	document.getElementById("to_"+link).classList.add("active");
 }
 
 // Au chargement de la page, on active l'élément du menu si l'url dispose d'une ancre
